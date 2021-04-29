068d902c031bc3f14039e6cbce959d373509626b
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-04-29 10:54:14 +0300

    Services Section

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,47 @@
+import home2 from '../img/home2.png';
+import { clock, diaphragm, money, teamwork } from '../img/icon';
+
+const ServicesSection = () => {
+  return (
+    <div className='services'>
+      <div className='description'>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <div className='cards'>
+          <div className='card'>
+            <div className='icon'>
+              <img src={clock} alt='icon' />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='card'>
+            <div className='icon'>
+              <img src={teamwork} alt='icon' />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='icon'>
+            <img src={diaphragm} alt='icon' />
+            <h3>Diaphragm</h3>
+          </div>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className='card'>
+          <div className='icon'>
+            <img src={money} alt='icon' />
+            <h3>Affordable</h3>
+          </div>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <img src={home2} alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
---
+++ src/img/icon/index.js
@@ -0,0 +1,6 @@
+import clock from './clock.svg';
+import diaphragm from './diaphragm.svg';
+import money from './money.svg';
+import teamwork from './teamwork.svg';
+
+export { clock, diaphragm, money, teamwork };
