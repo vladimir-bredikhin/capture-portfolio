02cfaa780a740ef5c993dd20bcdd8b8627734b89
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-05-04 15:44:04 +0300

    Intro To Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,68 @@
+import styled from 'styled-components';
 import home1 from '../img/home1.png';
+
 const AboutSection = () => {
   return (
-    <section className='about'>
-      <div className='description'>
+    <About>
+      <Description>
         <div className='title'>
-          <div className='hide'>
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography that you have. We have
           professionals with amazing skills
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className='image'>
+      </Description>
+      <Image>
         <img src={home1} alt='Camera Man' />
-      </div>
-    </section>
+      </Image>
+    </About>
   );
 };
+
+// Styled components
+
+const About = styled.section`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #ffffff;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
