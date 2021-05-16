7befad1481eda7ba7d88d17cb9714623a455b777
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-05-16 14:26:12 +0300

    About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import home1 from '../img/home1.png';
 import { Description, Hide, Image, Section } from '../styles';
+import { titleAnimation, fade, photoAnimation } from '../animation';
 
 const AboutSection = () => {
   return (
@@ -8,25 +9,25 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography that you have. We have
           professionals with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt='Camera Man' />
+        <motion.img variants={photoAnimation} src={home1} alt='Camera Man' />
       </Image>
     </Section>
   );
