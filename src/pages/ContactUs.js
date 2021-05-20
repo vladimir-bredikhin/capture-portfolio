dfdc2aebe6e87cced8cd181800e052c17351cde5
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-05-20 20:04:04 +0300

    Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,75 @@
 import { motion } from 'framer-motion';
 import React from 'react';
-import { page } from '../animation';
+import styled from 'styled-components';
+import { page, title } from '../animation';
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyled
       variants={page}
       initial='hidden'
       animate='show'
       exit='exit'
       style={{ background: '#fff' }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={title}>Get in touch </motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={title}>
+            <Circle />
+            <h2>Send Us A Message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={title}>
+            <Circle />
+            <h2>Send an email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={title}>
+            <Circle />
+            <h2>Social media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyled>
   );
 };
+
+const ContactStyled = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: #000000;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background-color: #353535;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
