387ac4d2afd8e070e4eb554fb6193189d7a11684
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-05-20 21:04:58 +0300

    Nav Line Animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
+import { motion } from 'framer-motion';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Nav = () => {
+  const { pathname } = useLocation();
+
   return (
     <StyledNav>
       <h1>
@@ -13,12 +16,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to='/'>1. About</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: 0 }}
+            animate={{ width: pathname === '/' ? '50%' : 0 }}
+          />
         </li>
         <li>
           <Link to='/work'>2. Our Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: 0 }}
+            animate={{ width: pathname === '/work' ? '50%' : 0 }}
+          />
         </li>
         <li>
           <Link to='/contact'>3. Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: 0 }}
+            animate={{ width: pathname === '/contact' ? '50%' : 0 }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -76,6 +94,19 @@ const StyledNav = styled.nav`
       justify-content: space-around;
       width: 100%;
     }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+
+  @media (max-width: 1300px) {
+    left: 0;
   }
 `;
 
